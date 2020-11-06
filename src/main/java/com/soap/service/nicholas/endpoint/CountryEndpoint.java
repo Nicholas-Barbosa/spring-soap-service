b9e2730ea4675cacec9086b0b846bf25e68bda7d
Nicholas-Barbosa <nicholas.henrique0604@hotmail.com>
2020-11-06 17:46:17 -0300

    Insert @Endpoint on class

--- src/main/java/com/soap/service/nicholas/endpoint/CountryEndpoint.java
+++ src/main/java/com/soap/service/nicholas/endpoint/CountryEndpoint.java
@@ -1,6 +1,7 @@
 package com.soap.service.nicholas.endpoint;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
 import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
 import org.springframework.ws.server.endpoint.annotation.RequestPayload;
 import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
@@ -10,6 +11,7 @@ import com.soap.service.nicholas.repository.CountryRepository;
 import io.spring.guides.gs_producing_web_service.GetCountryRequest;
 import io.spring.guides.gs_producing_web_service.GetCountryResponse;
 
+@Endpoint
 public class CountryEndpoint {
 
 	private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
